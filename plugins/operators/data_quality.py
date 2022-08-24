6646425d106dd16d6276af973ba4645eaa9f9ec6
crislaine <murilobiss@gmail.com>
2022-08-24 14:32:54 -0300

    fix: data quality changed

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -2,36 +2,47 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-
+"""
+Summary line. 
+DataQualityOperator creates a node in the dag 
+to count number of rows in the Redshift tables
+and raises ERROR when it encounters 0 rows. 
+"""
 class DataQualityOperator(BaseOperator):
-    """
-    Airflow Operator to perfrom data quality check on AWS Redshift tables
-    """
 
     ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
+                 # Define your operators params (with defaults) here
                  redshift_conn_id="",
                  tables=[],
                  *args, **kwargs):
-        
+
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        self.redshift_conn_id = redshift_conn_id
+        # Map params here
         self.tables = tables
+        self.redshift_conn_id = redshift_conn_id
 
     def execute(self, context):
-        redshift_hook = PostgresHook(self.redshift_conn_id)
-        
-        for table in self.tables:
+        redshift_hook = PostgresHook(self.redshift_conn_id)        
+        errors = []
+        self.log.info(self.tables)
+        for table in self.tables:            
             records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
             
             if len(records) < 1 or len(records[0]) < 1:
-                raise ValueError(f"Data quality check failed. {table} returned no results")
-                
+                errors.append(f"Data quality check failed. {table} returned no results")
+                                
             num_records = records[0][0]
+            self.log.info(f"Number of rows in {table} = {num_records}")
+            
             if num_records < 1:
-                raise ValueError(f"Data quality check failed. {table} contained 0 rows")
-
-            self.log.info(f"Data quality on table {table} check passed with {records[0][0]} records")
- 
+                errors.append(f"Data quality check failed. {table} contained 0 rows")            
+                
+        if len(errors) > 0:
+           for err in errors:
+               self.log.info(err)                
+           raise ValueError(f"Data quality check failed. Check log for more information")                
+        else:
+           self.log.info(f"Data quality on tables {self.tables} check passed.")
